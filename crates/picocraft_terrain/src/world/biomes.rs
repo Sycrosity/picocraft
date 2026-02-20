8a5f8fd11906020d9a702a1883fc20fb709f7d1d
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2026-02-20 15:21:08 +0000

    fix: add in other biome types, but disabled

--- crates/picocraft_terrain/src/world/biomes.rs
+++ crates/picocraft_terrain/src/world/biomes.rs
@@ -1,8 +1,18 @@
 use crate::prelude::*;
+
+// All biomes except plains currently don't work, since the shared registries
+// aren't implemented well yet, and onlu plains is defined.
 #[derive(Debug, picocraft_derive::Encode, Clone, Copy)]
 #[protocol(value = VarInt)]
 pub enum Biome {
     Plains = 0,
+    Ocean = -1,
+    Mountains = -2,
+    Desert = -3,
+    Taiga = -4,
+    Savanna = -5,
+    Forest = -6,
+    River = -7,
 }
 
 impl Biome {
