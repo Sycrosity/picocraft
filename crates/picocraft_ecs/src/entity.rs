90e63d8a18f036c81ba7da823be262016027bf1c
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2026-03-11 16:36:49 +0000

    fix: keep NonZeroU8 inside `EntityKind` and correctly handle index numbering

--- crates/picocraft_ecs/src/entity.rs
+++ crates/picocraft_ecs/src/entity.rs
@@ -13,36 +13,33 @@ impl EntityId {
     pub fn kind(&self) -> EntityKind {
         self.kind
     }
-    pub fn index(&self) -> NonZeroU8 {
-        self.index
+    pub fn index(&self) -> u8 {
+        self.index.get() - 1
+    }
+
+    pub fn new(kind: EntityKind, index: u8) -> Self {
+        assert!(index < u8::MAX, "Entity index must be less than 255");
+
+        Self {
+            kind,
+            index: NonZeroU8::new(index + 1).expect("Entity index should be non-zero"),
+        }
     }
 
     pub fn player(index: u8) -> Self {
-        Self {
-            kind: EntityKind::Player,
-            index: NonZeroU8::new(index).expect("Entity index must be non-zero"),
-        }
+        Self::new(EntityKind::Player, index)
     }
 
     pub fn mob(index: u8) -> Self {
-        Self {
-            kind: EntityKind::Mob,
-            index: NonZeroU8::new(index).expect("Entity index must be non-zero"),
-        }
+        Self::new(EntityKind::Mob, index)
     }
 
     pub fn item(index: u8) -> Self {
-        Self {
-            kind: EntityKind::Item,
-            index: NonZeroU8::new(index).expect("Entity index must be non-zero"),
-        }
+        Self::new(EntityKind::Item, index)
     }
 
     pub fn projectile(index: u8) -> Self {
-        Self {
-            kind: EntityKind::Projectile,
-            index: NonZeroU8::new(index).expect("Entity index must be non-zero"),
-        }
+        Self::new(EntityKind::Projectile, index)
     }
 }
 
@@ -63,7 +60,7 @@ impl EntityKind {
 
 pub struct EntityRef<'a, Pool> {
     pub pool: &'a mut Pool,
-    pub index: u8,
+    pub entity_id: EntityId,
 }
 
 impl<'a, Pool> EntityRef<'a, Pool> {
@@ -71,10 +68,10 @@ impl<'a, Pool> EntityRef<'a, Pool> {
         &mut self,
         component: C,
     ) -> Result<(), ComponentStorageError> {
-        component.insert_into(self.pool, self.index)
+        component.insert_into(self.pool, self.entity_id.index())
     }
 
     pub fn remove<C: RemoveFrom<Pool>>(&mut self) -> Result<(), ComponentStorageError> {
-        C::remove_from(self.pool, self.index)
+        C::remove_from(self.pool, self.entity_id.index())
     }
 }
